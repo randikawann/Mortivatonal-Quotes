74d1e273010297af38a8dcc0e7acda57c6da5744
randikawan <randikawann@gmail.com>
2019-07-04 09:36:39 +0530

    Initial Commit

---
+++ IOSMortivationQuotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSMortivationQuotes
+//
+//  Created by Randika on 7/4/19.
+//  Copyright © 2019 Xiteb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
